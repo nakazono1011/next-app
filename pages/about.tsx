f65234d4abce6f576c4a0ec13ef8269a90ba548d
nakazono1011 <syousanitiosi1011@yahoo.co.jp>
2023-05-14 16:23:36 +0900

    add onClick

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import { Inter } from "next/font/google";
+import Header from "@/components/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <Header />
+      <div>About Page</div>
+    </main>
+  );
+}
